4a9cc4b55123456f32ce2d5cdfb091c26df065e2
Eli-Src <elias.mail@tutanota.com>
2024-12-31 03:00:51 +0100

    Test change to nix

---
+++ nix-darwin/flake.nix
@@ -0,0 +1,104 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, mac-app-util, nix-homebrew, home-manager }:
+  let
+    configuration = { pkgs, ... }: {
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [
+        pkgs.neovim
+        pkgs.tmux
+        pkgs.nerd-fonts.jetbrains-mono
+
+        pkgs.home-manager
+
+        # Unfree
+        pkgs.obsidian
+        pkgs.reaper
+      ];
+
+      homebrew = {
+        enable = true;
+        casks = [
+          "librewolf"
+        ];
+        # --------------- IMPORTANT --------------------
+        # onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
+
+      system.defaults = {
+        dock.autohide = true;
+        loginwindow.GuestEnabled = false;
+        NSGlobalDomain.AppleICUForce24HourTime = true;
+        NSGlobalDomain.AppleInterfaceStyle = "Dark";
+        NSGlobalDomain.KeyRepeat = 2;
+      };
+      security.pam.enableSudoTouchIdAuth = true;
+
+      programs.zsh.enable = true;
+
+      users.users.elias.home = "/Users/elias";
+      home-manager.backupFileExtension = "backup";
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."core" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        mac-app-util.darwinModules.default
+        nix-homebrew.darwinModules.nix-homebrew {
+          nix-homebrew = {
+            enable = true;
+            # Apple Silicon Only
+            enableRosetta = true;
+            # User owning the Homebrew prefix
+            user = "elias";
+
+            # ----------------- TEMP -------------------
+            autoMigrate = true;
+          };
+        }
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.elias = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
